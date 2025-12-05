fed25d8422bab347ae779a33eba4a3ffcfaea585
Jelle <jellevandergraaf@live.com>
2025-12-05 08:33:15 +0100

    Disable debug mode for server startup

--- src/web_server.py
+++ src/web_server.py
@@ -1128,4 +1128,4 @@ if __name__ == '__main__':
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s: %(message)s'
     )
-    start_server(debug=True)
+    start_server(debug=False)
